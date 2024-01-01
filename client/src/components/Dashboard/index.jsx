4769a77dc45bec51513aad59866c5bb5b000e18c
Keshav Agrawal <keshava.bca2022@ssism.org>
2024-01-01 19:07:39 +0530

    dependency added in useEffect

--- client/src/components/Dashboard/index.jsx
+++ client/src/components/Dashboard/index.jsx
@@ -22,7 +22,7 @@ function Dashboard() {
         } else {
             fetchData();
         }
-    });
+    }, [isLoggedIn, navigate]);
 
     const fetchData = async () => {
         let response = "";
