5b2a981ce738b9c08b12114bb2bd402d272d8ae3
Keshav Agrawal <keshava.bca2022@ssism.org>
2024-01-02 08:20:26 +0530

    file upload fixed

--- server/middlewares/multer.middleware.js
+++ server/middlewares/multer.middleware.js
@@ -1,5 +1,11 @@
 const multer = require('multer');
 
-const upload = multer();
+const storage = multer.diskStorage({
+    filename: function (req, file, cb) {
+        cb(null, file.originalname + '-' + Date.now());
+    }
+});
+
+const upload = multer({ storage });
 
 module.exports = { upload };
